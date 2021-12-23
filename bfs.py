cf9e1bcc93951313968b493fc43db9db24852e2c
Euan Scott-Watson <37578825+EuanScottWatson@users.noreply.github.com>
2021-12-23 21:49:17 +0000

    BFS complete

---
+++ bfs.py
@@ -0,0 +1,37 @@
+class BFS:
+    def __init__(self, start, w, h) -> None:
+        self.visited = []
+        self.queue = [start]
+        self.w, self.h = w, h
+        self.parents = {}
+
+    def step(self):
+        next = self.queue.pop(0)
+        self.visited.append(next)
+
+        to_visit = [
+            (next[0]+1, next[1]),
+            (next[0], next[1]+1),
+            (next[0]-1, next[1]),
+            (next[0], next[1]-1)
+            ]
+
+        for n in to_visit:
+            if (n not in self.visited and 
+                n not in self.queue and 
+                0 <= n[0] <= self.w and 
+                0 <= n[1] <= self.h):
+                self.queue.append(n)
+                self.parents[n] = next
+        
+        return self.visited
+
+    def backtrace(self, end, start):
+        path = []
+        curr = end
+        while curr != start:
+            path.append(curr)
+            parent = self.parents[curr]
+            curr = parent
+        
+        return path
---
+++ visualise.py
@@ -0,0 +1,94 @@
+from bfs import BFS
+import pygame, os
+from pygame.locals import *
+import numpy as np
+
+
+class Path:
+    def __init__(self, w, h, grid_size=25):
+        self.width, self.height = w, h
+        self.grid_size = grid_size
+        self.grid = np.zeros((w, h))
+
+        self.start = (5, 10)
+        self.end = (20, 10)
+
+        self.done = False
+        self.visited = []
+        self.path = []
+
+        self.bfs = BFS(self.start, w, h)
+
+    def display(self, screen):
+        for i in range(self.width):
+            for j in range(self.height):
+                rect = pygame.Rect(i * self.grid_size, j * self.grid_size, self.grid_size, self.grid_size)
+                if (i, j) in self.visited:
+                    pygame.draw.rect(screen, (234, 255, 52), rect)
+                if (i, j) in self.path:
+                    pygame.draw.rect(screen, (0, 0, 0), rect)
+                if (i, j) == self.start:
+                    pygame.draw.rect(screen, (0, 255, 0), rect)
+                elif (i, j) == self.end:
+                    pygame.draw.rect(screen, (255, 0, 0), rect)
+
+        for i in range(self.width):
+            pygame.draw.line(screen, (0, 0, 0), (i * self.grid_size, 0), (i * self.grid_size, self.height * self.grid_size), 1)
+
+        for i in range(self.height):
+            pygame.draw.line(screen, (0, 0, 0), (0, i * self.grid_size), (self.width * self.grid_size, i * self.grid_size), 1)
+
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    return True
+
+    def display_screen(self, screen):
+        screen.fill((255, 255, 255))
+
+        self.display(screen)
+
+        pygame.display.update()
+        pygame.display.flip()
+
+    def run_logic(self):
+        if not self.done:
+            self.bfs_step()
+
+    def bfs_step(self):
+        self.visited = self.bfs.step()
+        if self.end in self.visited:
+            self.done = True
+            self.path = self.bfs.backtrace(self.end, self.start)
+        
+
+def main():
+    pygame.init()
+    pygame.font.init()
+    pygame.display.set_caption("Path Finding")
+
+    os.environ['SDL_VIDEO_CENTERED'] = "True"
+
+    width, height = 650, 650
+    grid_size = 25
+
+    screen = pygame.display.set_mode((width, height))
+
+    done = False
+    clock = pygame.time.Clock()
+    path = Path(width // grid_size, height // grid_size, grid_size=grid_size)
+
+    while not done:
+        done = path.events()
+        path.run_logic()
+        path.display_screen(screen)
+
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
